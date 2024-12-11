00b3c53cc0241a302c11122be497d433c91ba6ff
SadelaoudSaber <167130829+UniqueWhiter@users.noreply.github.com>
2024-12-11 11:16:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAof6bT060pZ1q1Lj57UuFKCxqMnUhc1XA",
+    authDomain: "devest2024-2f38c.firebaseapp.com",
+    projectId: "devest2024-2f38c",
+    storageBucket: "devest2024-2f38c.firebasestorage.app",
+    messagingSenderId: "107275103557",
+    appId: "1:107275103557:web:41b64c073b58ac2e84222c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
