ff884b5e1e208505e26012041fce3c00fd11f5c1
SadelaoudSaber <167130829+UniqueWhiter@users.noreply.github.com>
2024-12-21 13:20:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 // Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+import { initializeApp } from "";
+import { getDatabase } from "";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAof6bT060pZ1q1Lj57UuFKCxqMnUhc1XA",
-  authDomain: "devest2024-2f38c.firebaseapp.com",
-  projectId: "devest2024-2f38c",
-  storageBucket: "devest2024-2f38c.firebasestorage.app",
-  messagingSenderId: "107275103557",
-  appId: "1:107275103557:web:41b64c073b58ac2e84222c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
